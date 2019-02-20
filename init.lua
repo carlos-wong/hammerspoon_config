9d6d3080477b6cdc3379e0dba7fe6ded183f134b
carlos <huaixian.huang@gmail.com>
2019-02-20 20:19:13 +0800

    feat: keymap for https://raw.githubusercontent.com/manateelazycat/hammerspoon-config/master/init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,65 @@
+local inspect = require('inspect')
+local alert = require 'hs.alert'
+local application = require 'hs.application'
+local geometry = require 'hs.geometry'
+local grid = require 'hs.grid'
+local hints = require 'hs.hints'
+local hotkey = require 'hs.hotkey'
+local layout = require 'hs.layout'
+local window = require 'hs.window'
+local speech = require 'hs.speech'
+
+
+local hyper = {'ctrl', 'cmd'}
+
+
+local key2App = {
+  i = {'/Applications/iTerm.app', 'English', 2},
+  e = {'/Applications/Emacs.app', 'English', 2},
+  c = {'/Applications/Google Chrome.app', '', 2},
+  w = {'/Applications/WeChat.app', 'Chinese', 1},
+  f = {'/System/Library/CoreServices/Finder.app', 'English', 1},
+  s = {'/Applications/System Preferences.app', 'English', 1},
+}
+
+function findApplication(appPath)
+  local apps = application.runningApplications()
+  for i = 1, #apps do
+    local app = apps[i]
+    if app:path() == appPath then
+      return app
+    end
+  end
+
+  return nil
+end
+
+
+
+local function Chinese()
+  hs.keycodes.currentSourceID("com.sogou.inputmethod.sogou.pinyin")
+end
+
+local function English()
+  hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
+end
+
+function updateFocusAppInputMethod()
+  for key, app in pairs(key2App) do
+    local appPath = app[1]
+    local inputmethod = app[2]
+    if window.focusedWindow():application():path() == appPath then
+      if inputmethod == 'English' then
+        English()
+      elseif inputmethod ~= '' then 
+        Chinese()
+      end
+      break
+    end
+  end
+end
+
+
 function fullscreen()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -96,20 +158,6 @@ function reloadConfig(files)
 end
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 
-
-function launch_chrome()
-  hs.application.launchOrFocus("Google Chrome")
-end
-hs.hotkey.bind({"cmd", "ctrl"}, 'C', launch_chrome)
-hs.hotkey.bind({"cmd", "shift"}, 'C', launch_chrome)
-
-function launch_emacs()
-  hs.application.launchOrFocus("Emacs")
-end
-hs.hotkey.bind({"cmd", "ctrl"}, 'E', launch_emacs)
-hs.hotkey.bind({"cmd", "shift"}, 'E', launch_emacs)
-
-
 function launch_androidstudio()
   hs.application.launchOrFocus("Android Studio.app")
 end
@@ -117,28 +165,6 @@ hs.hotkey.bind({"cmd", "ctrl"}, 'A', launch_androidstudio)
 hs.hotkey.bind({"cmd", "shift"}, 'A', launch_androidstudio)
 
 
-function launch_wechat()
-  -- hs.alert.show("wechat_ctrl: " .. (hs.application.get("微信"):name() or "") .. string.format("bool: %s",hs.application.get("微信"):isFrontmost()) )
-  if not hs.application.get("微信") or not hs.application.get("微信"):isFrontmost() then
-    hs.application.launchOrFocus("Wechat")
-  else
-    hs.application.get("微信"):hide()
-  end
-end
-hs.hotkey.bind({"cmd", "ctrl"}, 'W', launch_wechat)
-hs.hotkey.bind({"cmd", "shift"}, 'W', launch_wechat)
-
-function launch_iterm()
-  hs.application.launchOrFocus("iTerm")
-end
-hs.hotkey.bind({"cmd", "ctrl"}, 'I', launch_iterm)
-hs.hotkey.bind({"cmd", "shift"}, 'I', launch_iterm)
-
-function launch_finder()
-  hs.application.launchOrFocus("Finder")
-end
-hs.hotkey.bind({"cmd", "ctrl"}, 'F', launch_finder)
-hs.hotkey.bind({"cmd", "shift"}, 'F', launch_finder)
 
 function applicationWatcher(appName, eventType, appObject)
   if (eventType == hs.application.watcher.activated) then
@@ -158,7 +184,109 @@ hs.hotkey.bind({"cmd","ctrl"}, "V", function() hs.eventtap.keyStrokes(hs.pastebo
 
 
 hs.urlevent.bind("CarlosAlert", function(eventName, params)
-                   -- print('debug params message:' .. params.message)
                    hs.alert.show(params.message)
 end)
 
+local carlosmenubar
+
+function Init()
+  carlosmenubar = hs.menubar.new()
+  carlosmenubar:setIcon("/Users/carlos/ownCloud/carlos_data/system-config/hammerspoon/hammerspoon_config/tomato.png")
+
+end
+
+Init()
+
+hs.urlevent.bind("CarlosUpdateMenubar", function(eventName, params)
+                   carlosmenubar:setTitle(params.message)
+                   carlosmenubar:setTooltip("123")
+end)
+
+
+-- Show launch application's keystroke.
+local showAppKeystrokeAlertId = ""
+
+local function showAppKeystroke()
+  if showAppKeystrokeAlertId == "" then
+    -- Show application keystroke if alert id is empty.
+    local keystroke = ""
+    local keystrokeString = ""
+    for key, app in pairs(key2App) do
+      keystrokeString = string.format("%-10s%s", key:upper(), app[1]:match("^.+/(.+)$"):gsub(".app", ""))
+
+      if keystroke == "" then
+        keystroke = keystrokeString
+      else
+        keystroke = keystroke .. "\n" .. keystrokeString
+      end
+    end
+
+    showAppKeystrokeAlertId = hs.alert.show(keystroke, hs.alert.defaultStyle, hs.screen.mainScreen(), 10)
+  else
+    -- Otherwise hide keystroke alert.
+    hs.alert.closeSpecific(showAppKeystrokeAlertId)
+    showAppKeystrokeAlertId = ""
+  end
+end
+
+hs.hotkey.bind(hyper, "z", showAppKeystroke)
+
+
+function launchApp(appPath)
+  application.launchOrFocus(appPath)
+end
+
+function toggleApplication(app)
+  local appPath = app[1]
+  local inputMethod = app[2]
+  local switchide = app[3]
+
+  -- Tag app path use for `applicationWatcher'.
+  startAppPath = appPath
+
+  local app = findApplication(appPath)
+  local setInputMethod = true
+
+  if not app then
+    -- Application not running, launch app
+    launchApp(appPath)
+  else
+    -- Application running, toggle hide/unhide
+    local mainwin = app:mainWindow()
+    if mainwin then
+      if app:isFrontmost() then
+        if switchide == 1 then
+          mainwin:application():hide()
+        end
+        setInputMethod = false
+      else
+        -- Focus target application if it not at frontmost.
+        mainwin:application():activate(true)
+        mainwin:application():unhide()
+        mainwin:focus()
+      end
+    else
+      -- Start application if application is hide.
+      if app:hide() then
+        launchApp(appPath)
+      end
+    end
+  end
+
+  if setInputMethod then
+    if inputMethod == 'English' then
+      English()
+    else
+      Chinese()
+    end
+  end
+end
+
+-- Start or focus application.
+for key, app, switchhide in pairs(key2App) do
+  hotkey.bind(
+    hyper, key,
+    function()
+      toggleApplication(app)
+  end)
+end
